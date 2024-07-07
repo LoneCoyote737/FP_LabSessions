f15913d8d9a993002972a772dcd3037f8950cb20
Drajuan <sp.john.manuel737@gmail.com>
2024-07-07 22:01:59 +0530

    finishedQ

--- Tutorial 4/q3.scala
+++ Tutorial 4/q3.scala
@@ -7,9 +7,12 @@ object Q3 extends App {
     def toLower (word: String) = word.toLowerCase()
     def formatNames (name: String)(format: String => String) = format(name)
 
-    val nameList = List("Benny", "Niroshan", "Saman", "Kumara")
+    val names = List("Benny", "Niroshan", "Saman", "Kumara")
     
-    println(nameList)
-    println(formatNames(nameList(0))(toUpper))
+    println(names)
 
+    println(formatNames(names(0))(toUpper))
+    println(s"${names(1).charAt(0)}${formatNames(names(1).charAt(1).toString())(toUpper)}${names(1).substring(2)}")
+    println(formatNames(names(2))(toLower))
+    println(s"${names(3).substring(0, names(3).length()-1)}${formatNames(names(3).last.toString())(toUpper)}")
 }
