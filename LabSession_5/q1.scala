83c429450e888ded10228f3d9151be4454b02c6b
Drajuan <sp.john.manuel737@gmail.com>
2024-07-15 23:04:32 +0530

    q1 done

---
+++ LabSession_5/q1.scala
@@ -0,0 +1,19 @@
+import scala.io.StdIn.readLine
+
+object Q1 extends App {
+    var productNames: List[String] = List.empty[String]
+
+    val getProductList = () => {
+        var input: String = ""
+        while ({input = readLine().trim(); input != "done"}) productNames = productNames :+ input
+    }
+    val printProductList = () => productNames.zipWithIndex.foreach((prod, i) => println(s"${i+1}. $prod"))
+    val getTotalProducts = () => productNames.length
+
+    // driver code:
+    println("Enter products' names ('done' to finish):")
+    getProductList()
+
+    printProductList()
+    println(s"Total in inventory: ${getTotalProducts()}")
+}
