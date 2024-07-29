ef4eb1da7a82b219241c635b3fd44d3bca3614d8
Drajuan <sp.john.manuel737@gmail.com>
2024-07-30 02:45:18 +0530

    done q2

---
+++ Tutorial_6/q2.scala
@@ -0,0 +1,56 @@
+// student record management with Scala tuples
+
+import scala.io.StdIn.*
+object Q2 extends App {
+
+    val getStudentInfo = () => {
+        println("Enter student's name: ")
+        val name = readLine()
+
+        println("Enter marks: ")
+        val marks = readInt()
+
+        println("Enter total possible marks: ")
+        val possMarks = readInt()
+
+        val studTupleH = (name, marks, possMarks)
+        if (validateInput(studTupleH)) {
+            val percent = (marks.toDouble / possMarks) * 100
+            val grade = findGrade(percent)
+            val studTuple = (name, marks, possMarks, percent, grade)
+            printStudentRecord(studTuple)
+        }
+        else getStudentInfoWithRetry(studTupleH)
+    }
+    val validateInput = (stud: (String, Int, Int)) => 
+        if ((stud(0).isEmpty) || (stud(1) < 0) || (stud(2) < 0)) false else true
+    val findGrade = (percent: Double) => percent match{
+        case x if x >= 90 => 'A'
+        case x if x >= 75 => 'B'
+        case x if x >= 50 => 'C'
+        case x  => 'D'
+    }
+    val printStudentRecord = (studTuple: (String, Int, Int, Double, Char)) => {
+        val (name, marks, possMarks, percent, grade) = studTuple
+        // println(s"Name: $studTuple(0)\n" +
+        //   s"Marks: $studTuple[1]\n" +
+        //   s"Total possible marks: $studTuple(2)\n" +
+        //   s"Percentage: $studTuple(3)\n" +
+        //   s"Grade: $studTuple(4)")
+        
+        println(s"\nName: $name\n" +
+          s"Marks: $marks\n" +
+          s"Total possible marks: $possMarks\n" +
+          s"Percentage: $percent\n" +
+          s"Grade: $grade")
+    }
+    val getStudentInfoWithRetry = (stTuple: (String, Int, Int)) => {
+        println("Invalid input, please try again!")
+        println(stTuple)
+        // getStudentInfo()
+    }
+    
+    
+    // driver code:
+    getStudentInfo()
+}
