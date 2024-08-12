77f009ae638be64ceb5fe0013cf3430063f19ac0
Drajuan <sp.john.manuel737@gmail.com>
2024-08-12 18:20:51 +0530

    done q2

--- Tutorial_8/q2.scala
+++ Tutorial_8/q2.scala
@@ -1,5 +1,17 @@
 // Integer pattern  matching
 
+import scala.io.StdIn
 object Q2 extends App {
+    println("Enter an integer:")
+    var myNum = scala.io.StdIn.readInt()
+
+    val multipleOf = (num: Int, div: Int) => num % div == 0
+
+    println(myNum match {
+        case x if multipleOf(x, 3) && multipleOf(x, 5) => "Multiple of Both Three and Five"
+        case x if multipleOf(x, 3) => "Multiple of Three"
+        case x if multipleOf(x, 5) => "Multiple of Five"
+        case x => "Not a Multiple of Three or Five"
+    })
 
 }
