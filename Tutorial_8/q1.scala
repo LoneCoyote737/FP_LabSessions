d17575ac7deabc3a718894807cd273cd2985a6fd
Drajuan <sp.john.manuel737@gmail.com>
2024-08-12 18:02:23 +0530

    done q1

--- Tutorial_8/q1.scala
+++ Tutorial_8/q1.scala
@@ -1,5 +1,28 @@
 // Ceaser cipher
 
 object Q1 extends App {
-    
+    def encryptCeaser (msg: String, shift: Int) = {
+        msg.map {
+            case 'z' => 'a'
+            case 'Z' => 'A'
+            case c if c.isLetter => (c + shift).toChar
+            case c => c
+        }
+    }
+    def decryptCeaser (msg: String, shift: Int) = {
+        msg.map {
+            case 'a' => 'z'
+            case 'A' => 'Z'
+            case c if c.isLetter => (c - shift).toChar
+            case c => c
+        }
+    }
+
+    var myString = "Attack Tolosa, seize survivors as slaves!";
+
+    var encrypted = encryptCeaser(myString, 1)
+    println("Encrypted msg:\n" + encrypted)
+
+    var decrypted = decryptCeaser(encrypted, 1)
+    println("Decrypted msg:\n" + decrypted)
 }
