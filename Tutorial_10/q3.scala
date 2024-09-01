28826c244d03e248cd509114068d6a7d69c24786
Drajuan <sp.john.manuel737@gmail.com>
2024-09-01 16:05:15 +0530

    some changes to q3, done q4

--- Tutorial_10/q3.scala
+++ Tutorial_10/q3.scala
@@ -4,33 +4,36 @@
 
 object Q3 extends App {
 
-    class Account (num: Int) {
+    class Account (val num: Int) {
         val accNum = num
         var accBalance: Int = 0 // assuming everything is in Int
 
         def deposit (amt: Int) = {
             this.accBalance = this.accBalance + amt
-            println(s"Amount deposited: $amt\n")
+            println(s"Account number: ${this.accNum} | Amount deposited: $amt\n")
         }
+
+        // commented parts are to prevent balance going to the negative
+
         def withdraw (amt: Int) = {
-            if (amt < this.accBalance) {
+            // if (amt < this.accBalance) {
                 this.accBalance = this.accBalance - amt
-                println(s"Amount withdrawn: $amt\n" +
-                        s"Remaining balance: ${this.accBalance}\n")
-            }
-            else println("Not enough balance in account!\n")
+                println(s"Account number: ${this.accNum}\n" +
+                        s"Amount withdrawn: $amt | Balance: ${this.accBalance}\n")
+            // }
+            // else println("Not enough balance in account!\n")
         }
         def transfer (amt: Int, transferee: Account) = {
-            if (amt < this.accBalance) {
+            // if (amt < this.accBalance) {
                 this.accBalance = this.accBalance - amt
                 transferee.accBalance = transferee.accBalance + amt
-                println(s"Amount transferred: $amt, " +
-                        s"To account number: ${transferee.accNum}\n" +
-                        s"Remaining balance: ${this.accBalance}")
-            }
-            else println("Not enough balance in account!\n")
+                println(s"Amount transferred: $amt\n" +
+                        s"From account: ${this.accNum} | To: ${transferee.accNum}\n" +
+                        s"Balance: ${this.accBalance}")
+            // }
+            // else println("Not enough balance in account!\n")
         }
-        override def toString: String = s"Account number: ${this.accNum}\nBalance remaining: ${this.accBalance}"
+        override def toString: String = s"Account number: ${this.accNum} | Balance: ${this.accBalance}"
     }
 
     val acc1 = new Account(123)
@@ -38,7 +41,7 @@ object Q3 extends App {
 
     acc1.deposit(5000)
     acc1.withdraw(1000)
-    acc2.withdraw(3000)
+    acc2.withdraw(2000)
     acc1.transfer(3000, acc2)
 
 }
--- Tutorial_10/q4.scala
+++ Tutorial_10/q4.scala
@@ -1,4 +1,29 @@
-
 object Q4 extends App {
+    class Account (val accNum: Int, var accBalance: Double) {
+        override def toString: String = s"Account number: $accNum | " +
+                                        s"Balance: $accBalance"
+    }
+
+    var Bank: List[Account] = List(Account(123, -2000), Account(234, 3000))
+
+    def negativeAccounts () = Bank.filter(_.accBalance < 0)
+    def sumBalance () = Bank.map(_.accBalance).sum
+
+    def applyInterest (): List[Account] = {
+        Bank.map{acc =>
+            val newBalance = if (acc.accBalance >= 0) acc.accBalance * 1.05
+                            else acc.accBalance * 1.1
+            new Account(acc.accNum, newBalance)
+        }
+    }
     
+    for(acc <- Bank) println(acc)
+
+    println("\nNegative accounts:")
+    for(acc <- negativeAccounts()) println(acc)
+
+    println(s"\nSum of balances: ${sumBalance()}")
+
+    println("\nAccount balances with interest:")
+    for(acc <- applyInterest()) println(acc)
 }
